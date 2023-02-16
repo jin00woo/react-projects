f691bd880c470e2e824c3d5a716e5d6db18f94e3
Jin Woo Han <jin00woo@naver.com>
2023-02-16 22:11:04 +0900

    Used context api to make components leaner.

---
+++ Typescript/react-with-ts/src/store/todos-context.tsx
@@ -0,0 +1,45 @@
+import Todo from "../models/todo";
+import React, { useState } from "react";
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: () => {},
+});
+
+type Props = {
+    children?: React.ReactNode
+};
+
+const TodosContextProvider: React.FC<Props> = ({children}) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
