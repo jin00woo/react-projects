3fa3878112186076f0d82028d95218dc30e79fdd
Jin Woo Han <jin00woo@naver.com>
2023-02-26 20:42:40 +0900

    Search bar
    Implemented logic for search bar component.

--- movie-app/src/hooks/useHomeFetch.js
+++ movie-app/src/hooks/useHomeFetch.js
@@ -10,6 +10,7 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -30,9 +31,12 @@ export const useHomeFetch = () => {
     }
     setLoading(false);
   };
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
 
-  return { state, loading, error };
+  // Initial and search
+  useEffect(() => {
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
+
+  return { state, loading, error, searchTerm, setSearchTerm };
 };
